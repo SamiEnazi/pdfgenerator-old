5657ab723c355fa519ae1f9b16f0799ac987118d
SamiEnazi <v.ipxc46540@gmail.com>
2023-08-04 22:25:42 +0300

    modified:   src/main/java/sa/stc/app/InputToHtml.java 	modified:   src/main/resources/pdf.pdf 	modified:   src/main/resources/pdf_templat.html 	new file:   src/main/resources/resources/styles.css 	modified:   src/main/resources/styles.css 	new file:   src/main/resources/templates/download_button.html

--- src/main/java/sa/stc/app/InputToHtml.java
+++ src/main/java/sa/stc/app/InputToHtml.java
@@ -1,11 +1,17 @@
 package sa.stc.app;
 
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,55 +23,71 @@ import com.github.jhonnymertz.wkhtmltopdf.wrapper.Pdf;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.configurations.WrapperConfig;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param;
 
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class InputToHtml {
-
-	@RequestMapping(value = "/create_pdf", method = RequestMethod.GET)
-	public ResponseEntity<String> createPDF() {
+	private static final Logger logger = LoggerFactory.getLogger(InputToHtml.class);
+	
+	@RequestMapping(value="/create_pdf",method=RequestMethod.GET)
+	public ResponseEntity<byte[]> createPDF() {
 		System.out.println("==================================================");
-
+		
+		
+		
 		try {
 			String executable = WrapperConfig.findExecutable();
 			Pdf pdf = new Pdf(new WrapperConfig(executable));
 			pdf.cleanAllTempFiles();
-			String htmlTemplate = StreamUtils.copyToString(
+			String htmlTemplate = StreamUtils.copyToString( 
 					new ClassPathResource("pdf_templat.html").getInputStream(),
-					Charset.defaultCharset());
-
-			// Map<String,String> map = new HashMap<String, String>();
-			// map.put("###InvoiceNum###", "عربي بحت");
-			// map.put("###Date###", LocalDateTime.now().toString());
-			// map.put("###Shipping###",String.valueOf(50.0));
-			// map.put("###Tax###",String.valueOf(10));
-			//
-			//
-			// htmlTemplate = util.replaceAllWithMap(htmlTemplate,map);
-
+					Charset.defaultCharset()
+					);
+			String css = StreamUtils.copyToString( 
+					new ClassPathResource("styles.css").getInputStream(),
+					Charset.defaultCharset()
+					);
+			
+//			Map<String,String> map = new HashMap<String, String>();
+//			map.put("###InvoiceNum###", "عربي بحت");
+//			map.put("###Date###",  LocalDateTime.now().toString());
+//			map.put("###Shipping###",String.valueOf(50.0));
+//			map.put("###Tax###",String.valueOf(10));
+//			
+//			
+//			htmlTemplate = util.replaceAllWithMap(htmlTemplate,map);
+			
+//			pdf.addParam(new com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--no-footer-line"), new com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--header-html", "file:///header.html"));
+//			pdf.addParam(new com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--enable-javascript"));
+//			pdf.addParam(new com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--javascript-delay", "2000"));
+			
 			pdf.addPageFromString(htmlTemplate);
-			// pdf.addParam(new
-			// com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--no-footer-line"),
-			// new com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--header-html",
-			// "file:///header.html"));
-			// pdf.addParam(new
-			// com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--enable-javascript"));
-			// pdf.addParam(new
-			// com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param("--javascript-delay",
-			// "2000"));
-
+			
+			//wkhtmltopdf --enable-local-file-access --dpi 300 --disable-smart-shrinking --margin-left 1mm --margin-right 1mm pdf_templat.html pdf.pdf
 			pdf.addParam(new Param("--enable-local-file-access"));
+			pdf.addParam(new Param("--dpi"),new Param("300"));
+			pdf.addParam(new Param("--disable-smart-shrinking"));
+			pdf.addParam(new Param("--margin-right"),new Param("1mm"));
+			pdf.addParam(new Param("--margin-left"),new Param("1mm"));
+			
+			
+			HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+	        headers.setContentDispositionFormData("attachment", "bill.pdf"); // Replace 'file.pdf' with the desired file name
 
-			pdf.addParam(new Param("--no-pdf-compression"));
-
+	        logger.info(System.getProperty("user.dir"));
 			File file = pdf.saveAs("src\\main\\resources\\pdf.pdf");
-
-			return new ResponseEntity<String>("[!] Done\n[!]Path:" + file.getAbsolutePath(), HttpStatus.OK);
-		} catch (InterruptedException | IOException e) {
+			return new ResponseEntity<>(pdf.getPDF(),headers,HttpStatus.OK);
+		} catch (InterruptedException|IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+
+		
+		
 		System.out.println("==================================================");
-		return new ResponseEntity<String>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>("Error".getBytes(StandardCharsets.UTF_8),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
